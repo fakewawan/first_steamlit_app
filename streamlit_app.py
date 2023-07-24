d7e0243ddbb7553df269b7247ee59fa1ccd44888
fakewawan <140448354+fakewawan@users.noreply.github.com>
2023-07-24 16:21:37 -0400

    Update streamlit_app.py
    items added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ streamlit.title('My parents new healthy diner!!');
 
 streamlit.header('Menu:');
 
+streamlit.text('Omega 3 & oatmeal');
 streamlit.text('Kale & Bullshit');
+streamlit.text('Hard boiled free range eggs');
