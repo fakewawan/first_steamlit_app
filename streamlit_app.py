c40c11a93085605f73704ba9e301f2cde2f43760
fakewawan <140448354+fakewawan@users.noreply.github.com>
2023-07-25 15:30:57 -0400

    Update streamlit_app.py
    stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit;
-import pandas;
+#import pandas;
 import requests;
 import snowflake.connector;
-
+from urllib.error import URLError;
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
@@ -37,6 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop();
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -46,3 +47,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('thanks for adding  ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('add_my_fruit')")
